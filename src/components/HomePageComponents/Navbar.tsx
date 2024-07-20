230dc58e540db7e26bb6e7df66bcb7288f7e98cd
gaurav <gauravpathak9058@gmail.com>
2024-07-21 01:08:04 +0530

    update

--- src/components/HomePageComponents/Navbar.tsx
+++ src/components/HomePageComponents/Navbar.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className=''>
-      <header className="p-4 navbarr">
-        <div className="container flex justify-between h-16 mx-auto">
-          <div className="flex justify-between w-full">
+    <div className=' '>
+      <header className="p-4  navbarr">
+        <div className="container flex justify-between   h-16 ">
+          <div className="flex justify-between w-full ">
             <Link  href="/" aria-label="Back to homepage" className="flex items-center p-2">
               <p className=' font-semibold text-3xl'>Cryptonite</p>
             </Link>
